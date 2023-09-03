3713d8b265bc298c72c74ac63e1e0b802b14f473
Derick Bond <obondoderick@gmail.com>
2023-09-03 17:06:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
- // Dynamically set the minimum height of the body to ensure it's at least 100vh
+  // Function to set the body's minimum height
     function setBodyMinHeight() {
       const headerHeight = document.querySelector('header').offsetHeight;
-      document.body.style.minHeight = `calc(100vh - ${headerHeight}px)`;
+      const windowHeight = window.innerHeight;
+      const body = document.body;
+
+      // Calculate the minimum height (100vh - headerHeight)
+      const minHeight = windowHeight - headerHeight;
+
+      // Set the body's minimum height
+      body.style.minHeight = minHeight + 'px';
     }
 
     // Call the function on page load and window resize
